4d5ef90f6cc5608eb3b4a146f6cf2f497d29e7d7
= <=>
2025-10-04 18:39:19 -0300

    implementação do login

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ password = os.getenv('PASSWORD')
 database = os.getenv('DATABASE')
 host = os.getenv('HOST')
 
+app = Flask(__name__)
+
 def connectarBanco():
     conexao = my.connect(
         user=user,
@@ -22,19 +24,40 @@ def connectarBanco():
 
 connectarBanco()
 
-app = Flask(__name__)
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
 
 @app.route('/', methods = ['GET','POST'])
-def index():
+def login():
     titulo = 'Página inicial'
     if request.method == 'POST':
         email = request.form.get('email')
         senha = request.form.get('senha')
         print(f'Email: {email}, senha: {senha}')
 
-        return render_template('index.html', titulo=titulo)
+        conexao = connectarBanco()
+        cursor = conexao.cursor(dictionary=True)
+        sql = 'select * from usuarios where email = %s'
+        cursor.execute(sql,(email,))
+        resultado = cursor.fetchone()
+        print(resultado)
+
+        if resultado:
+            if senha == resultado['senha']:
+                if resultado['tipo'] == 'cliente':
+                    return render_template('cliente.html', usuario=resultado)
+                elif resultado['tipo'] == 'admin':
+                    return render_template('admin.html', usuario=resultado)
+            else:
+                print('Senha incorreta')
+
+        else: 
+            print('Errou o email')
+            return render_template('login.html', titulo=titulo)
+        
     
-    return render_template('index.html', titulo=titulo)
+    return render_template('login.html', titulo=titulo)
 
 if __name__ == '__main__':
     app.run(debug=True)
